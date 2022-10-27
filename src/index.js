f0f423c984bee3ddf68ddb3cea7d3dec586b054b
Ashley Hart <ahart425@gmail.com>
2022-10-26 21:45:21 -0600

    started over with create react app, tailwind in installed and working

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from "react-dom";
-import './css/output.css';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const Index = () => {
-    return(
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Simple Tailwindo CSS application
-      </h1>
-      <button className="btn btn-blue">
-        Submit
-      </button>
-    </div>)
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-ReactDOM.render(<Index />, document.getElementById("index"));
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
